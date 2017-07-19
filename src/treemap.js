9d0657a936ebaf25681d19dde17bb7023dcf794b
karvannan thi <karvannanthi@gmail.com>
2017-07-19 12:33:05 +0530

    update c3 test file

--- src/treemap.js
+++ src/treemap.js
@@ -1,6 +1,4 @@
-import CLASS from './class';
 import { c3_chart_internal_fn } from './core';
-import { isFunction } from './util';
 
 c3_chart_internal_fn.initTreemap = function () {
     var $$ = this, d3 = $$.d3, config = $$.config;
